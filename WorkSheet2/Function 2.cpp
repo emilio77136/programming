fd1f62cf8f0d27cc7decdb788bdbb3062f670977
emilio77136 <2023158951@ub.edu.bz>
2025-02-04 21:57:51 -0600

    commit2

--- WorkSheet2/Function 2.cpp
+++ WorkSheet2/Function 2.cpp
@@ -4,9 +4,29 @@
 #include <iostream>
 using namespace std;
 
+int averageXd(int array[], int arraysize);
 
 int main(){
 
+int arraysize = 5;
+int array[arraysize] = {1,2,3,4,5};
+
+    int r = averageXd(array, arraysize);
+    cout << "Average: " << r;
 
     return 0;
+}
+
+int averageXd(int array[], int arraysize){
+
+    int sum = 0;
+    int average = 0;
+
+    for(int i = 0; i < arraysize; i++){
+        sum += array[i];
+    }
+
+    average = sum/arraysize;
+
+    return average;
 }
