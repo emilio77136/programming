3e32d98ec98864a843e46d765aa4f3c58f18da72
emilio77136 <2023158951@ub.edu.bz>
2025-02-04 21:45:24 -0600

    new codes

---
+++ WorkSheet2/Function 1.cpp
@@ -0,0 +1,30 @@
+//Sum of Elements: Write a function that takes an array and its size,
+//then returns the sum of its elements.
+
+
+#include <iostream>
+using namespace std;
+
+int sumXd(int array[], int arraysize);
+
+int main(){
+
+
+int arraysize = 5;
+int array[arraysize] = {1,2,3,4,5};
+
+int r = sumXd(array, arraysize);
+
+    return 0;
+}
+
+int sumXd(int array[], int arraysize){
+
+    int sum = 0;
+
+    for(int i = 0; i < arraysize; i++){
+        sum += array[i];
+    }
+
+    return sum;
+}
